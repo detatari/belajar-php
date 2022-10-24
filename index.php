7c4548e153d42c9c7bf4b0e6d4be1ff1b4fb2f86
Dewantari <dewantariie@gmail.com>
2022-10-24 11:07:36 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+
+echo "Hello World, It's <strong> Deta </strong>!<br>";
+$umur = "20";
+$nama = "SEAL AWS";
+$text = "PHP";
+
+echo "I am on my $umur now and currently joining $nama, this web is for practice $text. <br>";
+
+
+$namaKakak = "Daud";
+$umurKakak = "29";
+
+echo "I have a big bro named <strong>$namaKakak</strong>, my big brother is now on his 
+$umurKakak yo. <br>";
+
+$selisihumur = $umurKakak - $umur;
+
+echo "My brother and i are $selisihumur years apart"
+
+?>
